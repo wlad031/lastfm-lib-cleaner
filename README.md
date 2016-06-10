9b3f646836751aa842e48bd2a4e901450e10134f
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2016-06-10 20:11:43 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,29 @@
+##lastfm-lib-cleaner
+
+Clean your Last.fm scrobbles.
+
+###Usage
+
+####Clone repository:
+```
+git clone https://github.com/wlad031/lastfm-lib-cleaner lastfm-cleaner
+cd lastfm-cleaner/
+```
+
+####Install dependencies:
+```
+pip install -r requirements.txt
+```
+
+####Run
+```
+python app.py
+```
+
+Type your Last.fm login and password, the page numbers to clear, and wait for the end of the cleaning.
+
+
+####TO DO
+1. silent mode
+2. login/password retype
+
---
+++ app.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from getpass import getpass
+
+import sys
+
+
+lastfm_login_link = 'https://secure.last.fm/login'
+lastfm_library_link = 'http://www.last.fm/user/%s/library?page='
+
+js_script = """
+var x = document.getElementsByClassName(\"chartlist-delete-button\");
+for (var i = 0; i < x.length; i++) {
+    x[i].click();
+}
+"""
+
+
+def pages_input():
+    return
+
+if __name__ == '__main__':
+    username = raw_input('Last.fm login: ')
+    password = getpass('Password: ', stream=sys.stdout)
+
+    pages = raw_input('Library pages to clear (from-to): ').split('-')
+
+    while len(pages) != 2:
+        print('Invalid pages input!')
+        pages = raw_input('Library pages to clear (from-to): ').split('-')
+
+    page_from = int(pages[0])
+    page_to = int(pages[1])
+
+    ans = raw_input('Are you sure you want to clear your scrobbles on pages %s-%s (y/n)? ' %
+                    (str(page_from), str(page_to)))
+
+    if ans.upper() == 'Y':
+        link = lastfm_library_link % username
+
+        driver = webdriver.Chrome()
+        driver.get(lastfm_login_link)
+
+        username_form = driver.find_element_by_id("id_username")
+        password_form = driver.find_element_by_id("id_password")
+
+        username_form.send_keys(username)
+        password_form.send_keys(password)
+
+        driver.find_element_by_name("submit").click()
+
+        for i in range(page_to, page_from - 1, -1):
+            l = link + str(i)
+            driver.get(l)
+            driver.execute_script(js_script)
+
+        driver.close()
+
+        print('Your scrobbles cleared')
+
+    else:
+        exit(0)
